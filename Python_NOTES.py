ae7ef2a4129130c00f5b7d0132a13bb0a0c9ec90
PrestonRivera <prestonrivera211@gmail.com>
2024-05-04 21:51:49 -0500

    Update Python_NOTES.py

--- Python_NOTES.py
+++ Python_NOTES.py
@@ -323,11 +323,17 @@ def get_odd_numbers(num):
 
 # Slicing lists syntax example: my_list[ start : stop : step ]
 
-scores = [50, 70, 30, 20, 90, 10, 50]
-# Display list
+scores = [50, 70, 30, 20, 90, 10, 50] 
 print(scores[1:5:2])
 # Prints [70, 20]
 # The above reads as "give me a slice of the scores list from index 1, up to but not including 5, skipping every 2nd value. All of the sections are optional.
+
+# Splits the list of players into two seperate teams using slice with step values
+
+def split_players_into_teams(players):
+    even_team = players[0::2]
+    odd_team = players[1::2]
+    return even_team, odd_team
 
 # You can also omit various sections ("start", "stop", or "step"). For example, numbers[:3] means "get all items from the start up to (but not including) index 3". numbers[3:] means "get all items from index 3 to the end".
 
@@ -453,3 +459,393 @@ def get_first_item(items):
 
  def reverse_array(items):
     return items[-1::-1]
+
+
+
+# A list of the same messages but with all instances of the word dang removed.
+# A list containing the number of dang words that were removed from the message at that particular index.
+
+def filter_messages(messages):
+    filtered_messages = [] # Created empy list i can add to later on.
+    dang_count = []  # Created empy list i can add to later on.
+    
+    for messages in messages:
+        words = messages.split() # Splits the words in the string into seperate strings.
+        non_bad_words = []  # Empty list i can add to later.
+        counter = 0  # Counter for the word dang. 
+        
+        for words in words:
+            if words == "dang":  # Goes through strings and detects the word "dang"
+                counter += 1  # Adds to the counter for every word "dang"
+            else:
+                non_bad_words.append(words)  # Adds the clean words to the non_bad_words List
+
+        clean_message = " ".join(non_bad_words)  # Joins the seperate strings of non bad words into one string
+        filtered_messages.append(clean_message)  # Adds the clean words message to the final filtered_message list
+        dang_count.append(counter)  # adds the count of bad words to the dang_count list 
+    
+    return filtered_messages, dang_count  # Finally it returns the lists. 
+
+
+# The .split() method is called on a string. If you don't pass it any arguments, it will just split the words in the string on the whitespace.
+
+message = "hello there sam"
+words = message.split()
+print(words)
+# Prints: ["hello", "there", "sam"]
+
+
+# The .join() method is called on a delimiter (what goes between all the words in the list), and takes a list of strings as input.
+
+list_of_words = ["hello", "there", "sam"]
+sentence = " ".join(list_of_words)
+print(sentence)
+# Prints: "hello there sam"
+
+
+
+# Finding how many odd numbers and even numbers exist in a given list. 
+
+def get_odds_and_evens(numbers):
+    num_evens = 0
+    num_odds = 0
+
+
+    for i in range(0, len(numbers)):
+        if numbers[i] % 2 != 0:
+            num_odds += 1
+        else:
+            num_evens += 1
+    return num_odds, num_evens
+
+
+
+def get_test_score(answer_sheet, student_answers):
+    total_questions = len(answer_sheet)
+    correct_answers = 0
+
+    for answer, student_answer in zip(answer_sheet, student_answers):  # zip()Combinds two seperate iterables into one 
+        if answer == student_answer:
+            correct_answers += 1
+
+    percentage = (correct_answers / total_questions) * 100.0
+    return percentage
+
+
+def get_test_score(answer_sheet, student_answers):
+    total_questions = len(answer_sheet)
+    correct_answers = 0
+    name = student_answers.pop(0)
+
+    for correct_answer, student_answer in zip(answer_sheet, student_answers):
+        if correct_answer == student_answer:
+            correct_answers += 1
+
+    percentage = (correct_answers / total_questions) * 100 
+
+    return name, percentage
+
+
+# How to double letters in a string 
+
+def double_string(string):
+    new_sentence = ""
+    
+    for letter in string:
+        new_sentence += letter * 2
+    return new_sentence
+
+            
+# Dictionaries in Python are used to store data values in key -> value pairs. Dictionaries are a great way to store groups of information.
+# use curly braces
+# add key-value pairs
+
+car = {
+  "brand": "Tesla",
+  "model": "3",
+  "year": 2019
+}
+
+
+def get_character_record(name, server, level, rank):
+    record = {
+        "name": name,
+        "server": server,
+        "level": level,
+        "rank": rank,
+        "id": name + "#" + server # concantenates "name" with a number (#) with a server 
+    }
+
+    return record
+
+
+def get_character_record(name, server, level, rank):
+    return {
+        "name": name,
+        "server": server,
+        "level": level,
+        "rank": rank,
+        "id": f"{name}#{server}",
+    }
+
+
+# accessing the value in a dictionary by calling the key in square brackets [ ]
+
+car = {
+    'make': 'tesla',
+    'model': '3'
+}
+print(car['make'])
+# Prints: tesla
+
+
+names = ["jack bronson", "jill mcarty", "john denver"]
+
+names_dict = {}
+for name in names:
+    # .split() returns a list of strings
+    # where each string is a single word from the original
+    name_list = name.split()
+
+    # here we update the dictionary
+    names_dict[name_list[0]] = name_list[1]
+
+print(names_dict)
+# Prints: {'jack': 'bronson', 'jill': 'mcarty', 'john': 'denver'}
+
+
+# If you try to set the value of a key that already exists, you'll end up just updating the value of that key.
+# In this example, "denver" overwrote "bronson" as the value for the key "jack".
+
+full_names = ["jack bronson", "james mcarty", "jack denver"]
+
+names_dict = {}
+for full_name in full_names:
+    # .split() returns a list of strings
+    # where each string is a single word from the original
+    names = full_name.split()
+    first_name = names[0]
+    last_name = names[1]
+    names_dict[first_name] = last_name
+
+print(names_dict)
+# {
+#   'jack': 'denver',
+#   'james': 'mcarty'
+# }
+
+# You can delete existing keys using the del keyword.
+
+names_dict = {
+    'jack': 'bronson',
+    'jill': 'mcarty',
+    'joe': 'denver'
+}
+
+del names_dict['joe']
+
+print(names_dict)
+# Prints: {'jack': 'bronson', 'jill': 'mcarty'}
+
+# Notice that if you try to delete a key that doesn't exist, you'll get an error.
+
+names_dict = {
+    'jack': 'bronson',
+    'jill': 'mcarty',
+    'joe': 'denver'
+}
+
+del names_dict['unknown']
+# ERROR HERE, key doesn't exist
+
+# If you're unsure whether or not a key exists in a dictionary, use the in keyword.
+
+cars = {
+    'ford': 'f150',
+    'tesla': '3'
+}
+
+print('ford' in cars)
+# Prints: True
+
+print('gmc' in cars)
+# Prints: False
+
+# Checking for existence in a created dict. 
+
+def count_enemies(enemy_names):
+
+    enemy_name_dict = {}
+
+    for enemy_name in enemy_names:
+        if enemy_name in enemy_name_dict:
+          enemy_name_dict[enemy_name] += 1
+        else:
+            enemy_name_dict[enemy_name] = 1
+    return enemy_name_dict
+
+
+# iterating over a dictionary in python
+
+def get_most_common_enemy(enemies_dict): # CHAT
+
+    max_so_far = float("-inf")
+    most_common_enemy = None
+
+    for enemy, count in enemies_dict.items():
+        if count > max_so_far:
+            max_so_far = count
+            most_common_enemy = enemy
+    return most_common_enemy
+
+# ORRRRR
+
+def get_most_common_enemy(enemies_dict):
+
+    max_so_far = float("-inf")
+    most_common_enemy = None
+
+    for enemy_name in enemies_dict:
+        count = enemies_dict[enemy_name]
+        if count > max_so_far:
+            max_so_far = count
+            most_common_enemy = enemy_name
+    return most_common_enemy
+
+# Dictionaries in python version 3.6 and older were unordered. 3.7 and newwer are ordered and can i can iterate over dictionaries in the same order every time. 
+
+# Challenge showing how to access nested information in a dictionary.
+{
+    "type": {
+        "student": {
+            "name": "Allan",
+            "courses": {
+                "math_1050": {
+                    "current_grade": "B",
+                },
+                "English_1010": {
+                    "current_grade": "A-",
+                },
+            },
+        }
+    }
+}
+
+# Solution 
+
+def get_english_grade(student):
+    return student["type"]["student"]["courses"]["English_1010"]["current_grade"]
+
+
+# Total score challenge
+
+def merge(dict1, dict2): # k = key
+    merged_dict = {} 
+    for k in dict1:
+        merged_dict[k] = dict1[k]
+    for k in dict2:
+        merged_dict[k] = dict2[k]
+    return merged_dict
+
+def total_score(score_dict):
+    total = 0
+    for k in score_dict:
+        total += score_dict[k]
+    return total
+
+'''
+items_purchased: A list of the names of items purchased on this shopping trip. This is a list of strings.
+grocery_list: A list of the names of items Emma wanted to purchase. This is also a list of strings.
+
+OUTPUTS
+The function should return 3 values in this order:
+
+unpurchased_items: A list of all the item names in grocery_list that weren't found in items_purchased in order.
+receipt: A dictionary containing all the items Emma purchased, even stuff not on her list. The keys are the item names and the values are their respective prices from the item_prices dictionary.
+total: The total cost of all the items that were purchased.
+'''
+
+def calculate_total(items_purchased, grocery_list):
+    item_prices = {
+        "milk": 2.50,
+        "eggs": 3.25,
+        "bread": 1.21,
+        "cheese": 3.50,
+        "apples": 7.44,
+        "bananas": 3.88,
+        "carrots": 3.89,
+        "lettuce": 1.12,
+        "potatoes": 32.21,
+        "cereal": 5.99,
+    }
+
+
+    unpurchased_items = []
+    receipt = {}
+    total = 0
+
+    for item in grocery_list:
+        if item not in items_purchased:
+            unpurchased_items.append(item)
+
+    for item in items_purchased:
+        price = item_prices[item]
+        receipt[item] = price
+        total += price
+
+    return unpurchased_items, receipt, total
+
+
+
+# Sets are like Lists, but they are unordered and they guarantee uniqueness. Only ONE of each value can be in a set
+
+fruits = {'apple', 'banana', 'grape'}
+print(type(fruits))
+# Prints: <class 'set'>
+
+print(fruits)
+# Prints: {'banana', 'grape', 'apple'}
+
+# add values
+
+fruits = {'apple', 'banana', 'grape'}
+fruits.add('pear')
+print(fruits)
+# Prints: {'banana', 'grape', 'pear', 'apple'}
+
+# Note: No error will be raised if you add an item already in the set.
+
+# Because the empty bracket {} syntax creates an empty dictionary, to create an empty set, you need to use the set() function.
+
+fruits = set()
+fruits.add('pear')
+print(fruits)
+# Prints: {'pear'}
+
+# ITERATE OVER VALUES IN A SET (ORDER IS NOT GUARANTEED)
+
+fruits = {'apple', 'banana', 'grape'}
+for fruit in fruits:
+    print(fruit)
+    # Prints:
+    # banana
+    # grape
+    # apple
+
+
+    def remove_duplicates(spells):
+
+    learned_spells = set(spells) # I add the spells to a set. Removing any duplicates by adding to a set. 
+    com_spells = [] # created an empty list to add to later.
+    
+    for spells in learned_spells: # iterate over spells in the set.
+        com_spells.append(spells) # add spells to the com_spells list with removed duplicates. 
+    return com_spells
+
+# How to remove values from a set. my_set.remove("item")
+
+fruits = {'apple', 'banana', 'grape'}
+fruits.remove('apple')
+print(fruits)
+# Prints: {'banana', 'grape'}
+
