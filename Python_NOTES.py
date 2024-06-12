b7857aea502a905fc9d3011ee4894378fe3fedf4
PrestonRivera <prestonrivera211@gmail.com>
2024-06-11 22:05:18 -0500

    Updated notes from 6/11/24

--- Python_NOTES.py
+++ Python_NOTES.py
@@ -2624,3 +2624,65 @@ class Dragon(Unit):
 main()
 
 
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def get_area(self):
+        area = self.__length * self.__width
+        return area
+
+    def get_perimeter(self):
+        perimeter = 2 * (self.__length + self.__width)
+        return perimeter
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
+
+
+
+class Siege:
+    def __init__(self, max_speed, efficiency):
+        self.max_speed = max_speed
+        self.efficiency = efficiency
+
+    def get_trip_cost(self, distance, food_price):
+        trip_cost = (distance / self.efficiency) * food_price 
+        return trip_cost
+
+    def get_cargo_volume(self):
+        pass
+
+
+class BatteringRam(Siege):
+    def __init__(
+        self,
+        max_speed,
+        efficiency,
+        load_weight,
+        bed_area,
+    ):
+        super().__init__(max_speed, efficiency)
+        self.load_weight = load_weight
+        self.bed_area = bed_area
+
+    def get_trip_cost(self, distance, food_price):
+        base_cost = super().get_trip_cost(distance, food_price)
+        return base_cost + (self.load_weight * 0.01)
+
+    def get_cargo_volume(self):
+        volume = self.bed_area * 2
+        return volume
+
+
+class Catapult(Siege):
+    def __init__(self, max_speed, efficiency, cargo_volume):
+        super().__init__(max_speed, efficiency)
+        self.cargo_volume = cargo_volume
+
+    def get_cargo_volume(self):
+        return self.cargo_volume
